fc33ff3e94e168469af620087026a259be6f223b
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-08-11 08:47:19 +0530

    JavaScript Demo

---
+++ DemoJS.js
@@ -0,0 +1,47 @@
+//console.log("Niketan")
+/*console.log("Niketan")
+console.log("Niketan")
+console.log("Niketan")
+console.log("Niketan")*/
+
+let name = "Niketan";
+//let keyword is used to declare a variable
+console.log(name);
+
+var names = "Jenekar";
+//var is a lable that references a value
+console.log(names);
+
+const named = "Niketan Jenekar"
+//const is a read-only value
+console.log(named);
+
+//Datatype
+let number = 4;
+console.log(typeof(number));
+number = "Niketan";
+console.log(typeof(number));
+number = 5.32;
+console.log(typeof(number));
+
+//Array
+let array = new Array();
+let arrays = new Array(10);
+let arrr=new Array(1, 2, 3, 4, 5);
+let fruits=["Banana", "Apple"];
+console.log(fruits[0]);
+fruits[2]="Kiwi"
+console.log(fruits);
+console.log(fruits.length);
+fruits.push("Mango");
+console.log(fruits);
+fruits.unshift("Orange");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+
+console.log(fruits.indexOf("Apple"));
+console.log(Array.isArray("Apple"));
+
